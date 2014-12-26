2351672096b83a04147dfcfbc1b5097bd12dd5ab
Coyote <coyotealone@hotmail.fr>
2014-12-27 00:09:29 +0100

    0.1.9
    Update view activity_jeu

--- src/com/coyote/drinknomore/view/jeu/JeuActivity.java
+++ src/com/coyote/drinknomore/view/jeu/JeuActivity.java
@@ -1,9 +1,11 @@
 package com.coyote.drinknomore.view.jeu;
 
-import com.coyote.drinknomore.R;
-
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.coyote.drinknomore.R;
 
 public class JeuActivity extends Activity {
 
@@ -12,6 +14,12 @@ public class JeuActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_jeu);
+
+        //final EditText Enigme = (EditText) this.findViewById(R.id.jeu_txtEnigme);
+        //Enigme.setText("test");
+
+        //this.findViewById(R.id.jeu_txtEnigme)
+
 	}
 
 }
